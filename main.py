c8c47d706d3610d774ffe189c24163d04b269483
gynmixail <128672049+gynmixail@users.noreply.github.com>
2023-04-25 20:51:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+import os
+import random 
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def mysor(ctx):
+    gg = os.listdir('mysor')
+    r_images = random.choice(gg)
+    with open(f'mysor/{r_images}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem(ctx):
+    gg = os.listdir('images')
+    r_images = random.choice(gg)
+    with open(f'images/{r_images}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+bot.run("")
+
+
+
+
+
+
+
+
+
